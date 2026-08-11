d1b8d9292089a6483f00a1f93329af746c52bf0b
Rudra <rudradadhalwala@gmail.com>
2026-08-12 00:51:13 +0530

    Add Bit++ [A] from Codeforces

--- 282/A - Bit++/solution.java
+++ 282/A - Bit++/solution.java
@@ -2,30 +2,31 @@ import java.util.Scanner;
  
 public class Main {
     public static void main(String[] args) {
+        // Create a Scanner object to read input
         Scanner scanner = new Scanner(System.in);
         
-        // Check if there is an integer to read
-        if (scanner.hasNextInt()) {
-            int n = scanner.nextInt();
-            int x = 0;
+        // Read the number of statements
+        int n = scanner.nextInt();
+        
+        // Initialize the variable x
+        int x = 0;
+        
+        // Loop through all statements
+        for (int i = 0; i < n; i++) {
+            String statement = scanner.next();
             
-            // Loop through all statements
-            for (int i = 0; i < n; i++) {
-                String statement = scanner.next();
-                
-                // The '+' or '-' will always be at the middle index (1)
-                // for both X++ / ++X and X-- / --X
-                if (statement.charAt(1) == '+') {
-                    x++;
-                } else {
-                    x--;
-                }
+            // Check if the statement contains a '+'
+            if (statement.contains("+")) {
+                x++;
+            } else {
+                x--;
             }
-            
-            // Print the final result
-            System.out.println(x);
         }
         
+        // Print the final value
+        System.out.println(x);
+        
+        // Close the scanner
         scanner.close();
     }
 }
