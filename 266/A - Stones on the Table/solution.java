b64c93cc9322cf7c11b610ec09a86f889682d622
Rudra <rudradadhalwala@gmail.com>
2026-08-11 13:08:38 +0530

    Add Stones on the Table [A] from Codeforces

--- 266/A - Stones on the Table/solution.java
+++ 266/A - Stones on the Table/solution.java
@@ -1,26 +1,26 @@
 import java.util.Scanner;
  
-public class Main {
+public class StonesOnTheTable {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
         // Read the number of stones
         int n = scanner.nextInt();
-        // Read the string of colors
+        
+        // Read the string representing the stone colors
         String s = scanner.next();
         
-        int removals = 0;
+        int removeCount = 0;
         
-        // Loop through the string starting from the second stone
+        // Iterate through the string and compare adjacent stones
         for (int i = 1; i < n; i++) {
-            // If the current stone matches the previous one, it must be removed
             if (s.charAt(i) == s.charAt(i - 1)) {
-                removals++;
+                removeCount++;
             }
         }
         
-        // Output the total minimum removals
-        System.out.println(removals);
+        // Print the result
+        System.out.println(removeCount);
         
         scanner.close();
     }
