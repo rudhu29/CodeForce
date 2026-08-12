8ee7b67fdd31562985ada91c6e554e0a7de1f0eb
Rudra <rudradadhalwala@gmail.com>
2026-08-13 00:26:31 +0530

    Add Domino piling [A] from Codeforces

--- 50/A - Domino piling/solution.java
+++ 50/A - Domino piling/solution.java
@@ -3,11 +3,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        // Read the two integers M and N
         int m = scanner.nextInt();
         int n = scanner.nextInt();
         
+        // Calculate and print the maximum number of dominoes
         System.out.println((m * n) / 2);
         
+        // Close the scanner to prevent resource leaks
         scanner.close();
     }
 }
