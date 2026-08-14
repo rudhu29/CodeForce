1ab7f0eded4580857cde8e1765a0c53db0fd7ccf
Rudra <rudradadhalwala@gmail.com>
2026-08-14 21:11:41 +0530

    Add Translation [A] from Codeforces

--- 41/A - Translation/solution.java
+++ 41/A - Translation/solution.java
@@ -4,12 +4,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
+        // Read the two words
         String s = scanner.next();
         String t = scanner.next();
         
-        // Reverse string s and check if it equals t
+        // Reverse the first word using StringBuilder
         String reversedS = new StringBuilder(s).reverse().toString();
         
+        // Check if the reversed string matches t
         if (reversedS.equals(t)) {
             System.out.println("YES");
         } else {
