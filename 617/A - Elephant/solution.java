8c7eb2c13a1be83ff4a91699c0eeab210f84f423
Rudra <rudradadhalwala@gmail.com>
2026-08-17 01:19:08 +0530

    Add Elephant [A] from Codeforces

--- 617/A - Elephant/solution.java
+++ 617/A - Elephant/solution.java
@@ -4,12 +4,14 @@ public class Elephant {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
+        // Read the coordinate of the friend's house
         int x = scanner.nextInt();
         
-        // Ceiling division of x / 5
-        int minSteps = (x + 4) / 5;
+        // Calculate the minimum steps: equivalent to ceil(x / 5.0)
+        int steps = (x + 4) / 5;
         
-        System.out.println(minSteps);
+        // Print the result
+        System.out.println(steps);
         
         scanner.close();
     }
