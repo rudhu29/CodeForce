e88c04c80490bbb9d32d61b057cf225e4ad5d2fb
Rudra <rudradadhalwala@gmail.com>
2026-08-19 10:27:06 +0530

    Add Word [A] from Codeforces

--- 59/A - Word/solution.java
+++ 59/A - Word/solution.java
@@ -1,31 +1,25 @@
 import java.util.Scanner;
  
-public class WordCorrection {
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String s = scanner.next();
         
-        // Read the input word
-        if (scanner.hasNext()) {
-            String s = scanner.next();
-            
-            int upperCount = 0;
-            int lowerCount = 0;
-            
-            // Count uppercase and lowercase letters
-            for (int i = 0; i < s.length(); i++) {
-                if (Character.isUpperCase(s.charAt(i))) {
-                    upperCount++;
-                } else {
-                    lowerCount++;
-                }
-            }
-            
-            // Apply Vasya's rules
-            if (upperCount > lowerCount) {
-                System.out.println(s.toUpperCase());
+        int upperCount = 0;
+        int lowerCount = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (Character.isUpperCase(s.charAt(i))) {
+                upperCount++;
             } else {
-                System.out.println(s.toLowerCase());
+                lowerCount++;
             }
+        }
+        
+        if (upperCount > lowerCount) {
+            System.out.println(s.toUpperCase());
+        } else {
+            System.out.println(s.toLowerCase());
         }
         
         scanner.close();
