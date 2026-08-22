525cb610ce59a4b8798fd2050bdb0af187b64bf5
Rudra <rudradadhalwala@gmail.com>
2026-08-22 13:26:29 +0530

    Add Ultra-Fast Mathematician [A] from Codeforces

---
+++ 61/A - Ultra-Fast Mathematician/solution.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s1 = scanner.next();
+        String s2 = scanner.next();
+        
+        StringBuilder result = new StringBuilder();
+        
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                result.append('1');
+            } else {
+                result.append('0');
+            }
+        }
+        
+        System.out.println(result.toString());
+    }
+}
