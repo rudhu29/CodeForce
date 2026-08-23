fdf5917339159bd5486ef80a4cf41b6eb94b7f88
Rudra <rudradadhalwala@gmail.com>
2026-08-23 14:57:32 +0530

    Add Anton and Danik [A] from Codeforces

--- 734/A - Anton and Danik/solution.java
+++ 734/A - Anton and Danik/solution.java
@@ -1,28 +1,23 @@
 import java.util.Scanner;
  
-public class Main {
+public class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
-        // Read the number of games
         int n = scanner.nextInt();
-        
-        // Read the string of game outcomes
         String s = scanner.next();
-        
+        scanner.close();
+ 
         int antonWins = 0;
         int danikWins = 0;
-        
-        // Count the wins for both Anton and Danik
+ 
         for (int i = 0; i < n; i++) {
             if (s.charAt(i) == 'A') {
                 antonWins++;
-            } else if (s.charAt(i) == 'D') {
+            } else {
                 danikWins++;
             }
         }
-        
-        // Compare the results and print the winner
+ 
         if (antonWins > danikWins) {
             System.out.println("Anton");
         } else if (danikWins > antonWins) {
@@ -30,7 +25,5 @@ public class Main {
         } else {
             System.out.println("Friendship");
         }
-        
-        scanner.close();
     }
 }
