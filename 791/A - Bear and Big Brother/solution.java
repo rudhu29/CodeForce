7c1388721c9b0ce6885de075d0f959d2fe03584c
Rudra <rudradadhalwala@gmail.com>
2026-08-24 14:47:13 +0530

    Add Bear and Big Brother [A] from Codeforces

--- 791/A - Bear and Big Brother/solution.java
+++ 791/A - Bear and Big Brother/solution.java
@@ -1,29 +1,19 @@
 import java.util.Scanner;
  
-public class BearAndBigBrother {
-    public static int yearsToppingBrother(int a, int b) {
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
         int years = 0;
-        
+ 
         while (a <= b) {
             a *= 3;
             b *= 2;
             years++;
         }
-        
-        return years;
-    }
  
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        // Reading inputs a and b
-        if (scanner.hasNextInt()) {
-            int a = scanner.nextInt();
-            int b = scanner.nextInt();
-            
-            System.out.println(yearsToppingBrother(a, b));
-        }
-        
+        System.out.println(years);
         scanner.close();
     }
 }
