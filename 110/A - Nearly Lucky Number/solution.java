b9e221caecda777b872b4c94b781a7ce8e528507
Rudra <rudradadhalwala@gmail.com>
2026-08-26 12:43:12 +0530

    Add Nearly Lucky Number [A] from Codeforces

--- 110/A - Nearly Lucky Number/solution.java
+++ 110/A - Nearly Lucky Number/solution.java
@@ -3,26 +3,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
-        // Read the number as a String to easily iterate through its digits
-        if (scanner.hasNext()) {
-            String n = scanner.next();
-            int luckyCount = 0;
-            
-            // Count the occurrences of '4' and '7'
-            for (int i = 0; i < n.length(); i++) {
-                char digit = n.charAt(i);
-                if (digit == '4' || digit == '7') {
-                    luckyCount++;
-                }
+        String s = scanner.next();
+        int luckyCount = 0;
+ 
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '4' || c == '7') {
+                luckyCount++;
             }
-            
-            // Check if the count of lucky digits is a lucky number (4 or 7)
-            if (luckyCount == 4 || luckyCount == 7) {
-                System.out.println("YES");
-            } else {
-                System.out.println("NO");
-            }
+        }
+ 
+        if (luckyCount == 4 || luckyCount == 7) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
         }
         
         scanner.close();
