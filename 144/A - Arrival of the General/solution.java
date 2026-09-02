bb2d57414f07702f2d8e0bb36534d2e3f43b23d2
Rudra <rudradadhalwala@gmail.com>
2026-09-02 23:03:03 +0530

    Add Arrival of the General [A] from Codeforces

--- 144/A - Arrival of the General/solution.java
+++ 144/A - Arrival of the General/solution.java
@@ -4,27 +4,28 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        int[] a = new int[n];
-        
+        int max = 0;
+        int min = 101;
         int maxIndex = 0;
         int minIndex = 0;
-        
+ 
         for (int i = 0; i < n; i++) {
-            a[i] = scanner.nextInt();
-            if (a[i] > a[maxIndex]) {
+            int height = scanner.nextInt();
+            if (height > max) {
+                max = height;
                 maxIndex = i;
             }
-            if (a[i] <= a[minIndex]) {
+            if (height <= min) {
+                min = height;
                 minIndex = i;
             }
         }
-        
+ 
         int result = maxIndex + (n - 1 - minIndex);
         if (maxIndex > minIndex) {
             result--;
         }
-        
+ 
         System.out.println(result);
-        scanner.close();
     }
 }
