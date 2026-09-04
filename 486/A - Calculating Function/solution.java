8f11ed43382f2b490c3665393629dfad42aa4706
Rudra <rudradadhalwala@gmail.com>
2026-09-04 14:39:49 +0530

    Add Calculating Function [A] from Codeforces

--- 486/A - Calculating Function/solution.java
+++ 486/A - Calculating Function/solution.java
@@ -1,21 +1,15 @@
 import java.util.Scanner;
  
-public class CalculateFunction {
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
-        // Read n as a long since n can be up to 10^15
         long n = scanner.nextLong();
-        
-        long result;
-        if (n % 2 == 0) {
-            result = n / 2;
-        } else {
-            result = -(n + 1) / 2;
-        }
-        
-        System.out.println(result);
-        
         scanner.close();
+        
+        if (n % 2 == 0) {
+            System.out.println(n / 2);
+        } else {
+            System.out.println(-(n + 1) / 2);
+        }
     }
 }
