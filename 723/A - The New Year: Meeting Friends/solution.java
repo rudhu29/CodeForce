2cffc5ce93453c7fed882fa99c57bd6e5447d95e
Rudra <rudradadhalwala@gmail.com>
2026-09-15 21:05:17 +0530

    Add The New Year: Meeting Friends [A] from Codeforces

---
+++ 723/A - The New Year: Meeting Friends/solution.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.util.Arrays;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] x = new int[3];
+        x[0] = sc.nextInt();
+        x[1] = sc.nextInt();
+        x[2] = sc.nextInt();
+        
+        Arrays.sort(x);
+        System.out.println(x[2] - x[0]);
+    }
+}
