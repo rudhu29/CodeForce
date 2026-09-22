a2042bff7f24fc2eafe83ebaa366e93e12f62104
Rudra <rudradadhalwala@gmail.com>
2026-09-22 09:26:33 +0530

    Add Buy a Shovel [A] from Codeforces

---
+++ 732/A - Buy a Shovel/solution.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int k = scanner.nextInt();
+        int r = scanner.nextInt();
+ 
+        for (int i = 1; i <= 10; i++) {
+            int totalCost = i * k;
+            if (totalCost % 10 == 0 || totalCost % 10 == r) {
+                System.out.println(i);
+                break;
+            }
+        }
+ 
+        scanner.close();
+    }
+}
